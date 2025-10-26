e71be6575cbdd3828453f3ec9a9f2570a52a50cf
David Facó <132091233+David-Bugaboo@users.noreply.github.com>
2025-10-26 17:04:44 -0300

    Atualizar o SkillsPage.tsx

--- src/pages/campanha/SkillsPage.tsx
+++ src/pages/campanha/SkillsPage.tsx
@@ -13,10 +13,7 @@ function PowersPage() {
           <MobileSection>
             <PageTitle>Habilidades</PageTitle>
             <MobileText>
-              Os poderes são habilidades especiais que as figuras podem aprender
-              e usar durante o jogo. Cada poder tem um custo de ativação,
-              efeitos específicos e pode ser usado uma vez por jogo, a menos que
-              especificado de outra forma.
+              Habilidades são efeitos especiais que são aplicados a uma figura, permitindo que ela execute feitos ainda maiores em combate. Habilidades são ganhas a medida que a figura evolui, fazendo parte do seu crescimento durante uma campanha.
             </MobileText>
             <HeaderH1>A anatomia de uma habilidade</HeaderH1>
             <HeaderH2>Nome</HeaderH2>
@@ -25,17 +22,9 @@ function PowersPage() {
               seus efeitos.
             </MobileText>
             <HeaderH2>Gatilho</HeaderH2>
-            <MobileText>
-              O gatilho que permite o uso da habilidade. Lembre-se de que no
-              caso de múltiplas habilidades com o mesmo gatilho usadas ao mesmo
-              tempo, o jogador que está ativando atualmente escolhe a ordem de
-              resolução das habilidades.
-            </MobileText>
             <HeaderH2>Descrição</HeaderH2>
             <MobileText>
-              A descrição da habilidade e seus efeitos mais específicos, como
-              quanto de dado causa, o que faz com alvos que atinge e etcétera.
-              Pode ser uma descrição detalhada do efeito da habilidade.
+              A descrição da habilidade e seus efeitos mais específicos, como o que é melhorado no personagem.
             </MobileText>
           </MobileSection>
         </div>
